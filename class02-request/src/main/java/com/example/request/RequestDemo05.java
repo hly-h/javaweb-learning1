1b1c3f7b583e8394cfc1a4ee0c4d32771136b9ab
hly <2916291714@qq.com>
2024-09-10 17:25:18 +0800

    添加了class02

---
+++ class02-request/src/main/java/com/example/request/RequestDemo05.java
@@ -0,0 +1,19 @@
+package com.example.request;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/requestDemo05")
+public class RequestDemo05 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        super.doGet(req, resp);
+        System.out.println("访问requestDemo05");
+        req.getRequestDispatcher("/requestDemo06").forward(req,resp);
+    }
+}
